9975d4512c9cf64469e77f1f98d763cc5c9d03a1
xierensong <283318608@qq.com>
2018-09-12 22:08:53 +0800

    add xpath process

---
+++ case/xpath.py
@@ -0,0 +1,15 @@
+import requests
+from lxml import etree
+
+if __name__ == '__main__':
+    headers = {"User-Agent":'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+    url = 'https://www.apache.org/dist/ant/'
+    sourceHTML = requests.get(url, headers = headers)
+    selector = etree.HTML(sourceHTML.text)
+    folder_list = selector.xpath('//pre[position()=1]/a[@href]')
+    for elmt in folder_list:
+        dict = elmt.attrib
+        href_TT = dict['href']
+        print('href_TT ', href_TT)
+        if href_TT[len(href_TT)-1] == '/':
+            print('folder_list', elmt.attrib)
