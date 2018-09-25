dcaa24b3d78b5d481f598dbb202dbf0a204ced2e
xierensong <xierensong@github.com>
2018-09-25 19:04:50 +0800

    pom xml处理

--- case/xmlP.py
+++ case/xmlP.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from lxml import etree
 from io import StringIO
 
-xml_file = Path('../data/pom.xml')
+xml_file = Path('../data/pom2.xml')
 
 xml_content = xml_file.read_text()
 # print('xml content: ', xml_content)
@@ -16,7 +16,7 @@ xml_content = xml_file.read_text()
 # etree.tostring(xml_content, encoding='UTF-8')
 #xml_tree = etree.parse('../data/pom.xml')
 
-parser = etree.XMLParser(ns_clean=True,remove_blank_text=True)
+parser = etree.XMLParser(ns_clean=True, remove_blank_text=True)
 try:
     xml_tree = etree.parse('../data/pom1.xml')
 except etree.XMLSyntaxError as e:
@@ -29,7 +29,7 @@ root = xml_tree.getroot()
 # print(etree.tostring(root, pretty_print=True))
 # 获取groupId
 groupIds = []
-groupId_list = root.xpath('/ns:project/ns:build/ns:plugins/ns:plugin/ns:groupId',
+groupId_list = root.xpath('/ns:project/ns:dependencyManagement/ns:dependencies/ns:dependency/ns:groupId',
                            namespaces={'ns': 'http://maven.apache.org/POM/4.0.0'})
 for groupId in groupId_list:
     print('child', etree.tostring(groupId, pretty_print=True))
@@ -39,7 +39,7 @@ print('groupId length', len(groupIds))
 
 # 获取artifactId
 artifactIds = []
-artifactId_list = root.xpath('/ns:project/ns:build/ns:plugins/ns:plugin/ns:artifactId',
+artifactId_list = root.xpath('/ns:project/ns:dependencyManagement/ns:dependencies/ns:dependency/ns:artifactId',
                          namespaces={'ns': 'http://maven.apache.org/POM/4.0.0'})
 for artifactId in artifactId_list:
     print('child', etree.tostring(artifactId, pretty_print=True))
@@ -49,7 +49,7 @@ print('artifactId length', len(artifactIds))
 
 # 获取version
 versions = []
-version_list = root.xpath('/ns:project/ns:build/ns:plugins/ns:plugin/ns:version',
+version_list = root.xpath('/ns:project/ns:dependencyManagement/ns:dependencies/ns:dependency/ns:version',
                          namespaces={'ns': 'http://maven.apache.org/POM/4.0.0'})
 for version in version_list:
     print('child', etree.tostring(version, pretty_print=True))
@@ -61,7 +61,7 @@ print('version length', len(versions))
 # 获取plugin
 plugin_list = []
 plugin = {}
-plugins = root.xpath('/ns:project/ns:build/ns:plugins/ns:plugin',
+plugins = root.xpath('/ns:project/ns:dependencyManagement/ns:dependencies/ns:dependency',
                            namespaces={'ns': 'http://maven.apache.org/POM/4.0.0'})
 print('len', len(plugin_list))
 for i in range(len(plugins)):
@@ -96,13 +96,24 @@ for i in range(len(plugins)):
     if len(version_Id) == 0:
         plugin['version'] = 'N/A'
     else:
-        plugin['version'] = version_Id[0].text
+        version_Id_text = version_Id[0].text
+        if version_Id_text[0] == '$':
+            version_name = version_Id_text[2:-1]
+            # search for variety in xml
+            for version_v in root.iter('{http://maven.apache.org/POM/4.0.0}' + version_name):
+                print('version_v', version_v)
+                plugin['version'] = version_v.text
+        else:
+            plugin['version'] = version_Id_text
 
     print('plugin', dict(plugin))
 
     plugin_list.append(plugin)
 
 print('plugin_list length', len(plugin_list))
+
+for javac_version in root.iter('{http://maven.apache.org/POM/4.0.0}javac.version'):
+    print('javac_version:', javac_version.tag, javac_version.attrib, javac_version.text)
 
 
 
